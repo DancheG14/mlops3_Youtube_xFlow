7c111154051b2e01998f5d96659c7a56842283bb
ZAYKIN DANIIL <92274248+DancheG14@users.noreply.github.com>
2023-01-14 22:40:06 +0300

    Delete data_processing.py

--- data_processing.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
- 
-df = pd.read_csv('/data.csv', header=None)
- 
-df[0] = (df[0]-df[0].min())/(df[0].max()-df[0].min())
- 
-with open('/data_processed.csv', 'w') as f:
-    for i, item in enumerate(df[0].values):
-        f.write("{},{}\n".format(i, item))
